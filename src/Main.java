dc3a0a9fe5575ff56d06168648e5cb56f7c21b8a
cyber278a <cyber278a@gmail.com>
2023-03-30 14:19:45 -0400

    lights have been added

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,9 @@ public class Main {
         audi.model="A3";
         audi.color="black";
         audi.power="5000cc";
+        Lights neon=new Lights();
+        neon.color="red";
+        neon.size="656";
+        neon.intensity="6544l";
     }
 }
