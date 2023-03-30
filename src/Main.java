3ed8e4ce4589fe666f1328dfca922279f90fc088
cyber278a <cyber278a@gmail.com>
2023-03-30 12:06:31 -0400

    the engine specs can been added

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,10 @@ public class Main {
         audi.model="A3";
         audi.color="black";
         audi.power="5000cc";
+        Engine X=new Engine();
+        X.size="8";
+        X.size="234565";
+        X.weight="65544y";
+        
     }
 }
